f8786d1b7a1912fc595858cf1dc4f393f9e168f6
MTY777 <mataymekkurmanbek@gmail.com>
2022-01-12 20:09:37 +0600

    Initial commit

---
+++ src/com/company/BankAccount.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class BankAccount {
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void deposit(double sum) {
+        amount += sum;
+        System.out.println("Вы пополнили счет: " + sum);
+    }
+
+    public void withDraw(int sum) throws LimitException {
+        if (amount < sum) {
+            throw new LimitException("Операция не доступтно, потому что запрашиваемая сумма больше чем счет",
+                    amount);
+        } else {
+            amount -= sum;
+            System.out.println("Вы сняли со счета: " + sum);
+        }
+
+    }
+}
